2a53dbef12814d9118c2c77226afd371bb683fe0
Max Euler <maxjuliuseuler@gmail.com>
2019-12-14 10:20:09 +0100

    add webpack shell and nodemon to rebuild app on changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 const { NODE_ENV = 'production' } = process.env;
 module.exports = {
@@ -22,4 +23,10 @@ module.exports = {
     ],
   },
   externals: [nodeExternals()],
+  watch: NODE_ENV === 'development',
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildEnd: ['npm start'],
+    }),
+  ],
 };
